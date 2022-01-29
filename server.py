d25692cdbe6295431a76a8ea48d71cea926c4f8e
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-29 08:26:18 +0530

    updates

--- server.py
+++ server.py
@@ -2,3 +2,19 @@ import os
 import sys
 
 from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "hi"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=3334, debug=True)
