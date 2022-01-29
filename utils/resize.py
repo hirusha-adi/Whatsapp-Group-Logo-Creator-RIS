69fad553f5b5258ea199b9081aec8c0424f6b98b
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-29 08:51:41 +0530

    resize jpeg and png

--- utils/resize.py
+++ utils/resize.py
@@ -20,3 +20,39 @@ def jpg(filename: str, savename: str = "resized.jpg", width: int = 1024, height:
 
     imgjpg_resized = imgjpg.resize((width, height))
     imgjpg_resized.save(savename)
+
+
+def jpeg(filename: str, savename: str = "resized.jpeg", width: int = 1024, height: int = 1024):
+    if not(filename.lower().endswith(".jpeg")):
+        filename += ".jpeg"
+
+    if not(savename.lower().endswith(".jpeg")):
+        savename += ".jpeg"
+
+    try:
+        imgjpeg = Image.open(filename)
+    except FileNotFoundError:
+        sys.exit("No file found in " + str(os.getcwd()))
+    except Exception as e:
+        sys.exit("Error: " + str(e))
+
+    imgjpeg_resized = imgjpeg.resize((width, height))
+    imgjpeg_resized.save(savename)
+
+
+def png(filename: str, savename: str = "resized.png", width: int = 1024, height: int = 1024):
+    if not(filename.lower().endswith(".png")):
+        filename += ".png"
+
+    if not(savename.lower().endswith(".png")):
+        savename += ".png"
+
+    try:
+        imgpng = Image.open(filename)
+    except FileNotFoundError:
+        sys.exit("No file found in " + str(os.getcwd()))
+    except Exception as e:
+        sys.exit("Error: " + str(e))
+
+    imgpng_resized = imgpng.resize((width, height))
+    imgpng_resized.save(savename)
