a4293dfa9942983700c5f1a8ea28027b63289da4
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-29 08:26:02 +0530

    support

---
+++ utils/other.py
@@ -0,0 +1,18 @@
+import os
+
+if os.name == "nt":
+    CLEAR = "cls"
+    PIP = "pip"
+    PYTHON = "python"
+    SLASH = "\\"
+else:
+    CLEAR = "clear"
+    PIP = "pip3"
+    PYTHON = "python3"
+    SLASH = "/"
+
+__LOGO__ = r"""
+    
+    """
+
+__CREDITS__ = r"""  """
