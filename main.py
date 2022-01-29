4fd9a9564afd800b749987e28da22f9d0dca01f0
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-29 10:47:45 +0530

    bug fix

--- main.py
+++ main.py
@@ -28,28 +28,34 @@ def ENTIRE_PROGRAM():
         mm1 = input("[?] Select an option[1-3]: ")
 
         mm1_filename = input("[?] Enter file name: ")
-        mm1_savename = input("[?] Enter output save name [resized.jpg]: ")
-        mm1_width = input("[?] Enter width [1024]: ")
-        mm1_height = input("[?] Enter height [1024]: ")
+        mm1_savename = input("[?] Enter output save name [resized]: ")
+        try:
+            mm1_width = int(input("[?] Enter width [1024]: "))
+        except ValueError:
+            mm1_width = 1024
+        try:
+            mm1_height = int(input("[?] Enter height [1024]: "))
+        except ValueError:
+            mm1_height = 1024
 
         # Resize .jpg
         if mm1 == "1":
-            jpg(filename=mm1_filename, savename=mm1_savename,
-                width=int(mm1_width), height=int(mm1_height))
+            jpg(filename=mm1_filename, savename=mm1_savename if str(mm1_savename).lower().strip() else "result.jpg",
+                width=int(mm1_width), height=mm1_width)
 
         # Resize .jpeg
         elif mm1 == "2":
-            jpeg(filename=mm1_filename, savename=mm1_savename,
+            jpeg(filename=mm1_filename, savename=mm1_savename if str(mm1_savename).lower().strip() else "result.jpeg",
                  width=int(mm1_width), height=int(mm1_height))
 
         # Resize .png
         elif mm1 == "3":
-            png(filename=mm1_filename, savename=mm1_savename,
+            png(filename=mm1_filename, savename=mm1_savename if str(mm1_savename).lower().strip() else "result.png",
                 width=int(mm1_width), height=int(mm1_height))
 
         print("[+] Resized " + mm1_filename +
-              " to Width-" + mm1_width + ", Height-" + mm1_width +
-              " and saved to " + mm1_savename)
+              " to Width-" + str(mm1_width) + ", Height-" + str(mm1_width) +
+              " and saved to " + mm1_savename if str(mm1_savename).lower().strip() else "result")
         time.sleep(__SLEEP__)
 
 
