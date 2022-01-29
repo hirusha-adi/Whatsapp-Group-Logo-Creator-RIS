07450a32c345f29baebc6b078c77c0655d770e44
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-29 08:51:51 +0530

    manu for main.py added

--- utils/other.py
+++ utils/other.py
@@ -1,5 +1,30 @@
 import os
 
+__SLEEP__ = 3
+
+__LOGO__ = r"""
+ _____
+|__  /  Whatsapp Group Logo Creator
+  / /   for RIS Kurunegala
+ / /_   v1.0
+/____|  Made by @hirusha-adi
+"""
+
+__CREDITS__ = r"""
+Made by Hirusha Adikari
+"""
+
+__MAIN_MENU__ = r"""
+    [1] Start Web Server
+    [2] Resize Images 
+"""
+
+__ResizeMenu__ = r"""
+    [1] .jpg
+    [2] .jpeg
+    [3] .png
+"""
+
 if os.name == "nt":
     CLEAR = "cls"
     PIP = "pip"
@@ -10,9 +35,3 @@ else:
     PIP = "pip3"
     PYTHON = "python3"
     SLASH = "/"
-
-__LOGO__ = r"""
-    
-    """
-
-__CREDITS__ = r"""  """
