e402f0a4850a1718da4b3cec8ba3ee69577ed4be
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-30 08:24:43 +0530

    new /api route

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from flask import Flask, render_template, request, send_file
 
@@ -31,10 +30,44 @@ def index():
         return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
 
 
+@app.route("/api", methods=['GET', 'POST'])
+def api():
+    if request.method == 'GET':
+        topname = request.args.get('top', default=None)
+        middlename = request.args.get('middle', default=None)
+        bottomname = request.args.get('bottom', default=None)
+        stylename = request.args.get('style', default=None)
+        imagename2Edit = fixImageName(
+            imageName=request.args.get('image', default=None))
+        bgImg = os.path.join(os.getcwd(), "images", imagename2Edit)
+        if stylename == "1":
+            makeImg_Style1(title=topname, middle=middlename,
+                           bottom=bottomname, filename=bgImg, savename="result.jpg")
+        elif stylename == "2":
+            makeImg_Style2(title=topname, middle=middlename,
+                           bottom=bottomname, filename=bgImg, savename="result.jpg")
+        return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
+
+    if request.method == 'POST':
+        topname = request.form.get('topname')
+        middlename = request.form.get('middlename')
+        bottomname = request.form.get('bottomname')
+        stylename = request.form.get('stylename')
+        imagename2Edit = fixImageName(imageName=request.form.get('imagename'))
+        bgImg = os.path.join(os.getcwd(), "images", imagename2Edit)
+        if stylename == "1":
+            makeImg_Style1(title=topname, middle=middlename,
+                           bottom=bottomname, filename=bgImg, savename="result.jpg")
+        elif stylename == "2":
+            makeImg_Style2(title=topname, middle=middlename,
+                           bottom=bottomname, filename=bgImg, savename="result.jpg")
+        return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
+
+
 @ app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=3334, debug=True)
+    app.run("0.0.0.0", port=3334, debug=False)
