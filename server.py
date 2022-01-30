8ffd43c44fa02ef03b5c0ae9c950911f24e5315a
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-30 09:16:20 +0530

    removed POST support from /api

--- server.py
+++ server.py
@@ -30,38 +30,22 @@ def index():
         return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
 
 
-@app.route("/api", methods=['GET', 'POST'])
+@app.route("/api", methods=['GET'])
 def api():
-    if request.method == 'GET':
-        topname = request.args.get('top', default=None)
-        middlename = request.args.get('middle', default=None)
-        bottomname = request.args.get('bottom', default=None)
-        stylename = request.args.get('style', default=None)
-        imagename2Edit = fixImageName(
-            imageName=request.args.get('image', default=None))
-        bgImg = os.path.join(os.getcwd(), "images", imagename2Edit)
-        if stylename == "1":
-            makeImg_Style1(title=topname, middle=middlename,
-                           bottom=bottomname, filename=bgImg, savename="result.jpg")
-        elif stylename == "2":
-            makeImg_Style2(title=topname, middle=middlename,
-                           bottom=bottomname, filename=bgImg, savename="result.jpg")
-        return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
-
-    if request.method == 'POST':
-        topname = request.form.get('topname')
-        middlename = request.form.get('middlename')
-        bottomname = request.form.get('bottomname')
-        stylename = request.form.get('stylename')
-        imagename2Edit = fixImageName(imageName=request.form.get('imagename'))
-        bgImg = os.path.join(os.getcwd(), "images", imagename2Edit)
-        if stylename == "1":
-            makeImg_Style1(title=topname, middle=middlename,
-                           bottom=bottomname, filename=bgImg, savename="result.jpg")
-        elif stylename == "2":
-            makeImg_Style2(title=topname, middle=middlename,
-                           bottom=bottomname, filename=bgImg, savename="result.jpg")
-        return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
+    topname = request.args.get('top', default=None)
+    middlename = request.args.get('middle', default=None)
+    bottomname = request.args.get('bottom', default=None)
+    stylename = request.args.get('style', default=None)
+    imagename2Edit = fixImageName(
+        imageName=request.args.get('image', default=None))
+    bgImg = os.path.join(os.getcwd(), "images", imagename2Edit)
+    if stylename == "1":
+        makeImg_Style1(title=topname, middle=middlename,
+                       bottom=bottomname, filename=bgImg, savename="result.jpg")
+    elif stylename == "2":
+        makeImg_Style2(title=topname, middle=middlename,
+                       bottom=bottomname, filename=bgImg, savename="result.jpg")
+    return send_file('result.jpg', attachment_filename=f"{topname}-{middlename}-{bottomname}.jpg")
 
 
 @ app.errorhandler(404)
